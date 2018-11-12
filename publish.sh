6b5b907558c0bb3b3d2ed160ffc17fe98bd2ea78
Justin Gallardo <me@jirw.in>
2018-11-12 02:33:22 -0800

    Updates for new domain

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 copy_folder_to_s3(){
-	aws s3 cp $1 s3://hax.negativetrust.zone/ --recursive --region=us-east-1 --acl public-read
+	aws s3 cp $1 s3://hack.negativetrust.zone/ --recursive --region=us-east-1 --acl public-read
 }
 copy_folder_to_s3 $1
